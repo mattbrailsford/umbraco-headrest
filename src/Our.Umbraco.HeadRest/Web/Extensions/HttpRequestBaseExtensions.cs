73f9f783cfdb2b08e9c4f6e776713f86214f59e5
mattbrailsford <me@mattbrailsford.com>
2018-10-22 16:50:22 +0100

    Added strongly typed HeadRestRouteParam implementations

--- src/Our.Umbraco.HeadRest/Web/Extensions/HttpRequestBaseExtensions.cs
+++ src/Our.Umbraco.HeadRest/Web/Extensions/HttpRequestBaseExtensions.cs
@@ -1,20 +1,46 @@
 ﻿using Our.Umbraco.HeadRest.Web.Routing;
+using System;
+using System.Collections.Generic;
 using System.Web;
 
 namespace Our.Umbraco.HeadRest.Web.Extensions
 {
     public static class HttpRequestBaseExtensions
     {
-        public static string HeadRestRouteParam(this HttpRequestBase request, int index)
+        public static string HeadRestRouteParam(this HttpRequestBase request, int index, string defaultValue = null)
         {
             var routeParams = request.HeadRestRouteParams();
             return routeParams?[index];
         }
 
-        public static string HeadRestRouteParam(this HttpRequestBase request, string name)
+        public static TType HeadRestRouteParam<TType>(this HttpRequestBase request, int index, TType defaultValue = default(TType))
+        {
+            var param = request.HeadRestRouteParam(index);
+            if (param == null || string.IsNullOrWhiteSpace(param.ToString()))
+                return defaultValue;
+
+            var val = (TType)Convert.ChangeType(param, typeof(TType));
+            return val = !EqualityComparer<TType>.Default.Equals(val, default(TType))
+                ? val
+                : defaultValue;
+        }
+
+        public static string HeadRestRouteParam(this HttpRequestBase request, string name, string defaultValue = null)
         {
             var routeParams = request.HeadRestRouteParams();
             return routeParams?[name];
+        }
+
+        public static TType HeadRestRouteParam<TType>(this HttpRequestBase request, string name, TType defaultValue = default(TType))
+        {
+            var param = request.HeadRestRouteParam(name);
+            if (param == null || string.IsNullOrWhiteSpace(param.ToString()))
+                return defaultValue;
+
+            var val = (TType)Convert.ChangeType(param, typeof(TType));
+            return val = !EqualityComparer<TType>.Default.Equals(val, default(TType))
+                ? val
+                : defaultValue;
         }
 
         private static HeadRestRouteParamsCollection HeadRestRouteParams(this HttpRequestBase request)
