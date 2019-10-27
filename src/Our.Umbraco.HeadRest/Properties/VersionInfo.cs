af1e69a156930fe247687dff53162cc5c1a5c83b
mattbrailsford <me@mattbrailsford.com>
2019-10-27 09:15:46 +0000

    Getting appveyor to work

--- src/Our.Umbraco.HeadRest/Properties/VersionInfo.cs
+++ src/Our.Umbraco.HeadRest/Properties/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyInformationalVersion("0.0.0-alpha-000100")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha-000100")]
 
 
